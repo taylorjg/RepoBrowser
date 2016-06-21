3f1a67afc7d5a93e53b3a6777a8eec31901006a2
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-06-21 18:28:06 +0100

    onSubmit2 => onSubmit

--- scripts/searchForm.directive.js
+++ scripts/searchForm.directive.js
@@ -13,17 +13,15 @@
             templateUrl: 'scripts/searchForm.directive.html',
             replace: true,
             scope: {
-                onSubmit2: '&'
+                onSubmit: '&'
             },
-            controller: function ($scope) {
+            controller: function () {
                 var vm = this;
                 vm.token = '';
                 vm.username = 'taylorjg';
                 vm.filter = '';
-                vm.onSubmit = function () {
-                    console.log('searchForm.onSubmit');
-                    console.log(vm.onSubmit2);
-                    vm.onSubmit2({
+                vm.onFormSubmit = function () {
+                    vm.onSubmit({
                         token: vm.token,
                         username: vm.username,
                         filter: vm.filter
